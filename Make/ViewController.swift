105a62b6fbbc7f3311776d6e622025eddff04df2
Ebubechukwu Dimobi <dimobiblaise@gmail.com>
2021-12-15 13:24:47 +0300

    add love (+1 squashed commit) Squashed commits: [ded26fa] remove all
    jjjj

---
+++ Make/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Make
+//
+//  Created by Ebubechukwu Dimobi on 15.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //
+        // 1
+        // don't reemove
+        // remove
+        
+        // try
+        // try
+        //
+        //
+        //
+        
+        // okay
+        
+        // test
+        // test
+        //test2
+    }
+
+
+}
+
