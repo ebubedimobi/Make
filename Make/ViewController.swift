c67e95f02318d3ae55421e4539a5668b4cf7fffb
Ebubechukwu Dimobi <dimobiblaise@gmail.com>
2021-12-15 13:24:47 +0300

    top

---
+++ Make/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Make
+//
+//  Created by Ebubechukwu Dimobi on 15.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //
+        // 1
+        // don't reemove
+        // remove
+        
+        // try
+        // try
+        
+        // okay
+    }
+// i test
+
+}
+
+
